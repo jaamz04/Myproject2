1584c441fd42c685f3adb82194a9aeaa11add33d
jaamz04 <jamesarpilang04@gmail.com>
2025-03-21 00:40:05 +0800

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,255 @@
+const jwt = require("jsonwebtoken");
+const { signupSchema, signinSchema, acceptCodeSchema, changePasswordSchema,acceptFPCodeSchema } = require("../middlewares/validator");
+const User = require('../models/userModel');
+const { doHash, doHashValidation, hmacProcess } = require('../utils/hashing');
+const transport = require("../middlewares/sendMail");
+
+exports.signup = async(req, res)=>{
+   const {email,password} = req.body;
+   try {
+    const {error, value}= signupSchema.validate({email,password});
+
+    if(error){
+        return res.status(401).json({success:false, message: error.details[0].message})
+    }
+
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(401).json({success:false, message:"User Already Exist!"})
+    }
+
+    const hashedPassword = await doHash(password,12);
+    const newUser = new User({
+        email,
+        password:hashedPassword,
+    })
+    const result = await newUser.save();
+    result.password = undefined;
+    res.status(201).json({
+        success:true, message:"Your Account has been Created Sucessfully!", result
+    })
+
+
+   } catch (error) {
+    console.log(error)
+   }
+};
+
+exports.signin =async(req, res)=>{
+    const{email, password} = req.body;
+
+    try {
+        const {error, value}= signinSchema.validate({email,password});
+
+        if(error){
+            return res.status(401).json({success:false, message: error.details[0].message})
+        }
+    
+        const existingUser = await User.findOne({email}).select('+password')
+        if(!existingUser){
+            return res.status(401).json({success:false, message:"User does not Exist!"})
+        }
+
+        const result = await doHashValidation(password, existingUser.password)
+        if(!result){
+            return res.status(401).json({success:false, message:"Invalid Credentials!"})
+        }
+
+        const token = jwt.sign({
+            userId:existingUser._id,
+            email:existingUser.email,
+            verified: existingUser.verified,
+        },
+        process.env.TOKEN_SECRET,{
+            expiresIn:'8h',
+        }
+       
+    );
+
+    res.cookie('Authorization', 'Bearer' + token, {expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production'}).json({
+        success:true,
+        token,
+        mesage: 'logged in successfully',
+
+    });
+    } catch (error) {
+        console.log(error);
+    }
+}
+exports.signout = async (req, res)=>{
+    res.clearCookie('Authirization').status(200).json({success:true, message: "Logged Out Successfully"})
+}
+
+exports.sendVerificationCode = async (req, res)=>{
+    const {email}=req.body;
+
+    try {
+        const existingUser = await User.findOne({email})
+        if(!existingUser){
+            return res.status(404).json({success:false, message:"User does not Exist!"})
+        }
+        if(existingUser.verified){
+            return res.status(400).json({success:false, message:"You already Verified!"})
+        }
+        // sendcode to that email using math function
+        const codeValue = Math.floor(Math.random() * 10000000).toString();
+
+        let info = await transport.sendMail({
+            from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+            to: existingUser.email,
+            subject: "Verification Code",
+            html: '<h1>'+ codeValue +'</h1>'
+        })
+
+        if(info.accepted[0]=== existingUser.email){
+            const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET)
+            existingUser.verificationCode = hashedCodeValue;
+            existingUser.verificationCodeValidation = Date.now();
+            await existingUser.save()
+            return res.status(200).json({success:true, message: "Code sent"})
+        }
+        res.status(400).json({success:true, message: "Code sent Failed!"});
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.verifyVerificationCode = async (req, res)=>{
+    const {email, providedCode}= req.body;
+
+    const {error, value}= acceptCodeSchema.validate({email,providedCode});
+
+    if(error){
+        return res.status(401).json({success:false, message: error.details[0].message})
+    }
+    const codeValue = providedCode.toString();
+    const existingUser = await User.findOne({email}).select("+verificationCode verificationCodeValidation");
+
+    if(!existingUser){
+        return res.status(401).json({success:false, message:"User does not Exist!"})
+    }
+    if(existingUser.verified){
+        return res.status(400).json({success:false, message:"You already Verified!"})
+    }
+    if(!existingUser.verificationCode || !existingUser.verificationCodeValidation){
+        return res.status(400).json({success:false, message:"Something is wrong with the Code!"})
+    }
+    // send code 5 minutes if dili makavalidate will throw error
+    if (Date.now() - existingUser.verificationCodeValidation > 5 * 60 * 1000) {
+        return res.status(400).json({success:false, message:"Code has been expired!"})
+    }
+    const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET)
+    if(hashedCodeValue === existingUser.verificationCode){
+        existingUser.verified = true;
+        existingUser.verificationCode = undefined;
+        existingUser.verificationCodeValidation = undefined;
+        await existingUser.save();
+        return res.status(200).json({success:true, message:"Your Account has been verified!"})
+    }
+    return res.status(400).json({success:false, message:"Unexpected Occur!"})
+
+}
+
+exports.changePassword = async (req, res)=>{
+    const {userId, verified} = req.user;
+    const {oldPassword, newPassword} = req.body;
+
+    try {
+        const {error, value}= changePasswordSchema.validate({oldPassword, newPassword});
+        if(error){
+            return res.status(401).json({success:false, message: error.details[0].message})
+        }
+
+        const existingUser = await User.findOne({_id:userId}).select('+password');
+        if(!existingUser){
+            return res.status(401).json({success:false, message:"User does not Exist!"})
+        }
+
+        const result = await doHashValidation(oldPassword, existingUser.password);
+        if(!result){
+            return res.status(401).json({success:false, message:"Invalid Credentials!"})
+        }
+
+        const hashedPassword = await doHash(newPassword,12);
+        existingUser.password = hashedPassword;
+        await existingUser.save();
+        return res.status(200).json({success:true, message:"Password Updated!"});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+exports.sendForgotPasswordCode = async (req, res)=>{
+    const {email}=req.body;
+
+    try {
+        const existingUser = await User.findOne({email})
+        if(!existingUser){
+            return res.status(404).json({success:false, message:"User does not Exist!"})
+        }
+       
+        // sendcode to that email using math function
+        const codeValue = Math.floor(Math.random() * 10000000).toString();
+
+        let info = await transport.sendMail({
+            from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+            to: existingUser.email,
+            subject: "Forgot Password Code",
+            html: '<h1>'+ codeValue +'</h1>'
+        })
+
+        if(info.accepted[0]=== existingUser.email){
+            const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET);
+            existingUser.forgotPasswordCode = hashedCodeValue;
+            existingUser.forgotPasswordCodeValidation = Date.now();
+            await existingUser.save()
+            return res.status(200).json({success:true, message: "Code sent"})
+        }
+        res.status(400).json({success:true, message: "Code sent Failed!"});
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.verifyForgotPasswordCode = async (req, res)=>{
+    const {email, providedCode, newPassword}= req.body;
+
+    const {error, value}= acceptFPCodeSchema.validate({email,providedCode,newPassword});
+
+    if(error){
+        return res.status(401).json({success:false, message: error.details[0].message})
+    }
+    const codeValue = providedCode.toString();
+    const existingUser = await User.findOne({email}).select("+forgotPasswordCode +forgotPasswordCodeValidation");
+
+    if(!existingUser){
+        return res.status(401).json({success:false, message:"User does not Exist!"})
+    }
+    
+    if(!existingUser.forgotPasswordCode || !existingUser.forgotPasswordCodeValidation){
+        return res.status(400).json({success:false, message:"Something is wrong with the Code!"})
+    }
+    // send code 5 minutes if dili makavalidate will throw error
+    if (Date.now() - existingUser.forgotPasswordCodeValidation > 5 * 60 * 1000) {
+        return res.status(400).json({success:false, message:"Code has been expired!"})
+    }
+    const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET);
+
+    if(hashedCodeValue === existingUser.forgotPasswordCode){
+        const hashedPassword = await doHash(newPassword,12);
+        existingUser.password = hashedPassword;
+        existingUser.forgotPasswordCode = undefined;
+        existingUser.forgotPasswordCodeValidation = undefined;
+        await existingUser.save();
+        return res.status(200).json({success:true, message:"Password Updated!"})
+    }
+    return res.status(400).json({success:false, message:"Unexpexted Occur!"})
+
+}
+
---
+++ models/userModel.js
@@ -0,0 +1,44 @@
+const { required, number } = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:[true, 'Email is required!'],
+        trim: true,
+        unique: [true, "Email must be Unique!"],
+        minLenght: [5, "Email must have 5 Characters!"],
+        lowercase: true,
+    },
+    password:{
+        type:String,
+        required:[true, 'Password is required!'],
+        trim: true,
+        select:false,
+    },
+    verified:{
+        type:Boolean,
+        default:false,
+
+    },
+    verificationCode:{
+        type: String,
+        select:false,
+    },
+    verificationCodeValidation:{
+        type: Number,
+        select:false,
+    },
+    forgotPasswordCode:{
+        type: String,
+        select:false,
+    },
+    forgotPasswordCodeValidation:{
+        type: Number,
+        select:false,
+    }
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model("User", userSchema);
